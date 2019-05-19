b1faa2af18adaa8075100b5c64d237d1ec9f2268
Matt A. Tobin <email@mattatobin.com>
2019-05-19 13:02:30 -0400

    Move global functions back to basicFunctions and include it in index.php

--- modules/basicFunctions.php
+++ modules/basicFunctions.php
@@ -3,90 +3,250 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// == | Function: funcError | =================================================
+/**********************************************************************************************************************
+* Basic Content Generation using the Special Component's Template
+***********************************************************************************************************************/
+function funcGenerateContent($aTitle, $aContent, $aTextBox = null, $aList = null, $aError = null) {
+  $templateHead = @file_get_contents('./components/special/skin/default/template-header.xhtml');
+  $templateFooter = @file_get_contents('./components/special/skin/default/template-footer.xhtml');
 
-// This function simply relays an error message and dies
-function funcError($_value) {
-  ob_get_clean();
-  header('Content-Type: text/html', false);   
-  print(file_get_contents('./skin/default/template-header.xhtml'));
-  print('<h2>' . $GLOBALS['strProductName'] . ' ' . $GLOBALS['strApplicationVersion'] . '</h2>');
-  print('<p class="pulseText" style="text-decoration: blink;"><strong>Fatal Error</strong></p>');
-  print('<ul><li>' . $_value . '</li></ul>');
-  print(file_get_contents('./skin/default/template-footer.xhtml'));
-  
-  // We are done here
+  // Make sure the template isn't busted, if it is send a text only error as an array
+  if (!$templateHead || !$templateFooter) {
+    funcError([__FUNCTION__ . ': Special Template is busted...', $aTitle, $aContent], -1);
+  }
+
+  // Can't use both the textbox and list arguments
+  if ($aTextBox && $aList) {
+    funcError(__FUNCTION__ . ': You cannot use both textbox and list');
+  }
+
+  // Anonymous function to determin if aContent is a string-ish or not
+  $notString = function() use ($aContent) {
+    return (!is_string($aContent) && !is_int($aContent)); 
+  };
+
+  // If not a string var_export it and enable the textbox
+  if ($notString()) {
+    $aContent = var_export($aContent, true);
+    $aTextBox = true;
+    $aList = false;
+  }
+
+  // Use either a textbox or an unordered list
+  if ($aTextBox) {
+    // We are using the textbox so put aContent in there
+    $aContent = '<textarea style="width: 1195px; resize: none;" name="content" rows="36" readonly>' .
+                $aContent .
+                '</textarea>';
+  }
+  elseif ($aList) {
+    // We are using an unordered list so put aContent in there
+    $aContent = '<ul><li>' . $aContent . '</li><ul>';
+  }
+
+  // Set page title
+  $templateHead = str_replace('<title></title>',
+                  '<title>' . $aTitle . ' - ' . SOFTWARE_NAME . ' ' . SOFTWARE_VERSION . '</title>',
+                  $templateHead);
+
+  // If we are generating an error from funcError we want to clean the output buffer
+  if ($aError) {
+    ob_get_clean();
+  }
+
+  // Send an html header
+  header('Content-Type: text/html', false);
+
+  // write out the everything
+  print($templateHead . '<h2>' . $aTitle . '</h2>' . $aContent . $templateFooter);
+
+  // We're done here
   exit();
 }
 
-// ============================================================================
+/**********************************************************************************************************************
+* Error function that will display data (Error Message)
+**********************************************************************************************************************/
+function funcError($aValue, $aMode = 0) {
+  $varExport  = var_export($aValue, true);
+  $jsonEncode = json_encode($aValue, 448); // JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+  
+  $pageHeader = array(
+    'default' => 'Unable to Comply',
+    'fatal'   => 'Fatal Error',
+    'php'     => 'PHP Error',
+    'output'  => 'Output'
+  );
 
-// == | Function: funcHTTPGetValue |===========================================
-
-// This function gets HTTP GET arguments and performs /very/ basic filtering
-// or returns a predictable null
-function funcHTTPGetValue($_value) {
-  if (!isset($_GET[$_value]) || $_GET[$_value] === '' ||
-    $_GET[$_value] === null || empty($_GET[$_value])) {
-    return null;
+  switch($aMode) {
+    case -1:
+      // Text only
+      header('Content-Type: text/plain', false);
+      if (is_string($aValue) || is_int($aValue)) {
+        print($aValue);
+      }
+      else {
+        print($varExport);
+      }
+      break;
+    case 1: funcGenerateContent($pageHeader['php'], $aValue, null, true, true);
+            break;
+    // Deprecated, use funcGenerateContent
+    case 98: funcGenerateContent($pageHeader['output'], $jsonEncode, true);
+             break;
+    // Deprecated, use funcGenerateContent
+    case 99: funcGenerateContent($pageHeader['output'], $varExport, true);
+             break;
+    default: funcGenerateContent($pageHeader['default'], $aValue, null, true, true);
   }
-  else {  
-    $_finalValue =
-      preg_replace('/[^-a-zA-Z0-9_\-\/\{\}\@\.\%\s]/', '', $_GET[$_value]);
-    return $_finalValue;
+
+  exit();
+}
+
+/**********************************************************************************************************************
+* PHP Error Handler
+**********************************************************************************************************************/
+
+function funcPHPErrorHandler($errno, $errstr, $errfile, $errline) {
+  $errorCodes = array(
+    E_ERROR => 'Fatal Error',
+    E_WARNING => 'Warning',
+    E_PARSE => 'Parse',
+    E_NOTICE => 'Notice',
+    E_CORE_ERROR => 'Fatal Error (Core)',
+    E_CORE_WARNING => 'Warning (Core)',
+    E_COMPILE_ERROR => 'Fatal Error (Compile)',
+    E_COMPILE_WARNING => 'Warning (Compile)',
+    E_USER_ERROR => 'Fatal Error (User Generated)',
+    E_USER_WARNING => 'Warning (User Generated)',
+    E_USER_NOTICE => 'Notice (User Generated)',
+    E_STRICT => 'Strict',
+    E_RECOVERABLE_ERROR => 'Fatal Error (Recoverable)',
+    E_DEPRECATED => 'Depercated',
+    E_USER_DEPRECATED => 'Depercated (User Generated)',
+    E_ALL => 'All',
+  );
+
+  $errorType = $errorCodes[$errno] ?? $errno;
+  $errorMessage = $errorType . ': ' . $errstr . ' in ' .
+                  str_replace(ROOT_PATH, '', $errfile) . ' on line ' . $errline;
+
+  if (error_reporting() !== 0) {
+    funcError($errorMessage, 1);
   }
 }
 
-// ============================================================================
+set_error_handler("funcPHPErrorHandler");
 
-// == | Function: funcCheckVar | ==============================================
+/**********************************************************************************************************************
+* Unified Var Checking
+*
+* @param $_type           Type of var to check
+* @param $_value          GET/PUT/SERVER/FILES/EXISTING Normal Var
+* @param $_allowFalsy     Optional - Allow falsey returns (really only works with case var)
+* @returns                Value or null
+**********************************************************************************************************************/
+function funcUnifiedVariable($_type, $_value, $_allowFalsy = null) {
+  $finalValue = null;
 
-// This function is good for truely knowing if an /existing/ variable has
-// a useable value or returns a predictable null
-function funcCheckVar($_value) {
-  if ($_value === '' || $_value === 'none' || $_value === null || empty($_value)) {
+  switch ($_type) {
+    case 'get':
+      $finalValue = $_GET[$_value] ?? null;
+
+      if ($finalValue) {
+        $finalValue = preg_replace('/[^-a-zA-Z0-9_\-\/\{\}\@\.\%\s\,]/', '', $_GET[$_value]);
+      }
+
+      break;
+    case 'post':
+      $finalValue = $_POST[$_value] ?? null;
+      break;
+    case 'server':
+      $finalValue = $_SERVER[$_value] ?? null;
+      break;
+    case 'files':
+      $finalValue = $_FILES[$_value] ?? null;
+      if ($finalValue) {
+        if (!in_array($finalValue['error'], [UPLOAD_ERR_OK, UPLOAD_ERR_NO_FILE])) {
+          funcError('Upload of ' . $_value . ' failed with error code: ' . $finalValue['error']);
+        }
+
+        if ($finalValue['error'] == UPLOAD_ERR_NO_FILE) {
+          $finalValue = null;
+        }
+        else {
+          $finalValue['type'] = mime_content_type($finalValue['tmp_name']);
+        }
+      }
+      break;
+    case 'cookie':
+      $finalValue = $_COOKIE[$_value] ?? null;
+      break;
+    case 'var':
+      $finalValue = $_value ?? null;
+      break;
+    default:
+      funcError('Incorrect var check');
+  }
+
+  if (!$_allowFalsy && (empty($finalValue) || $finalValue === 'none' || $finalValue === '')) {
     return null;
   }
-  else {
-    return $_value;
-  }
+
+  return $finalValue;
 }
 
-// ============================================================================
+/**********************************************************************************************************************
+* Check if a module is in $arrayIncludes
+*
+* @param $_value    A module
+* @returns          true or null depending on if $_value is in $arrayIncludes
+**********************************************************************************************************************/
+function funcCheckModule($_value) {
+  if (!array_key_exists('arrayIncludes', $GLOBALS)) {
+    funcError('$arrayIncludes is not defined');
+  }
+  
+  if (!in_array($_value, $GLOBALS['arrayIncludes'])) {
+    return null;
+  }
+  
+  return true;
+}
 
-// == | funcSendHeader | ======================================================
-
-// This function allows easy sending of common header types
+/**********************************************************************************************************************
+* Sends HTTP Headers to client using a short name
+*
+* @param $_value    Short name of header
+**********************************************************************************************************************/
 function funcSendHeader($_value) {
   $_arrayHeaders = array(
-    '404' => 'HTTP/1.0 404 Not Found',
-    '501' => 'HTTP/1.0 501 Not Implemented',
-    'html' => 'Content-Type: text/html',
-    'text' => 'Content-Type: text/plain',
-    'xml' => 'Content-Type: text/xml',
-    'css' => 'Content-Type: text/css',
-    'phoebus' => 'X-Phoebus: https://github.com/Pale-Moon-Addons-Team/phoebus/',
+    '404'           => 'HTTP/1.0 404 Not Found',
+    '501'           => 'HTTP/1.0 501 Not Implemented',
+    'html'          => 'Content-Type: text/html',
+    'text'          => 'Content-Type: text/plain',
+    'xml'           => 'Content-Type: text/xml',
+    'json'          => 'Content-Type: application/json',
+    'css'           => 'Content-Type: text/css',
+    'phoebus'       => 'X-Phoebus: https://github.com/Pale-Moon-Addons-Team/phoebus/',
   );
   
-  if (array_key_exists($_value, $_arrayHeaders)) {
+  if (!headers_sent() && array_key_exists($_value, $_arrayHeaders)) {
     header($_arrayHeaders['phoebus']);
     header($_arrayHeaders[$_value]);
     
-    if ($_value == '404') {
+    if ($_value == '404' || $_value == '501') {
       // We are done here
       exit();
     }
   }
-  else {
-    // Fallback to text
-    header($_arrayHeaders['text']);
-  }
 }
 
-// ============================================================================
-
-// == | Function: funcRedirect |===============================================
-
+/**********************************************************************************************************************
+* Sends HTTP Header to redirect the client to another URL
+*
+* @param $_strURL   URL to redirect to
+**********************************************************************************************************************/
 // This function sends a redirect header
 function funcRedirect($_strURL) {
 	header('Location: ' . $_strURL , true, 302);
@@ -95,63 +255,33 @@ function funcRedirect($_strURL) {
   exit();
 }
 
-// ============================================================================
+// --------------------------------------------------------------------------------------------------------------------
 
-// == | Function: funcRealArray | =============================================
-
-// Sometimes classes and crap can send array-like objects instead of true
-// arrays.. This uses a quick and dirty method of converting them to real arrays
-// using json encode/decode.. IF the input is not an object or array OR json
-// fails it will return null (we hope)
-function funcRealArray($_value, $_isMD = true) {
-  if (is_object($_value) || is_array($_value)) {
-    if ($_isMD == true) {
-      $_result = json_decode(json_encode($_value), true);
-    }
-    else {
-      $_result = json_decode(json_encode($_value));
-    }
-  }
-  else {
-    return null;
-  }
-  
-  return $_result;
-}
-
-// ============================================================================
-
-// == | Function: funcCheckUserAgent |=========================================
-
-// YEAH well.. bite me!
-function funcCheckUserAgent() {
-  if (startsWith(strtolower($_SERVER['HTTP_USER_AGENT']), 'wget/') ||
-    startsWith(strtolower($_SERVER['HTTP_USER_AGENT']), 'curl/')) {
+/**********************************************************************************************************************
+* Sends a 404 error but does it depending on debug mode
+***********************************************************************************************************************/
+function funcSend404() {
+  if (!$GLOBALS['arraySoftwareState']['debugMode']) {
     funcSendHeader('404');
   }
+  funcError('404 - Not Found');
 }
 
-// ============================================================================
+/**********************************************************************************************************************
+* Polyfills for missing functions
+* startsWith, endsWith, contains
+*
+* @param $haystack  string
+* @param $needle    substring
+* @returns          true if substring exists in string else false
+**********************************************************************************************************************/
 
-// == | Function: funcPrintVar |===============================================
-
-// Does something I manually typed too many time...
-function funcPrintVar($_var) {
-  ob_get_clean();
-  funcSendHeader('text');
-  var_export($_var);
-  die();
-}
-
-// ============================================================================
-
-// == | Functions: startsWith, endsWith, contains |============================
-
-// These functions are stolen.. They may be suboptimal but are very useful
 function startsWith($haystack, $needle) {
    $length = strlen($needle);
    return (substr($haystack, 0, $length) === $needle);
 }
+
+// --------------------------------------------------------------------------------------------------------------------
 
 function endsWith($haystack, $needle) {
   $length = strlen($needle);
@@ -162,6 +292,8 @@ function endsWith($haystack, $needle) {
   return (substr($haystack, -$length) === $needle);
 }
 
+// --------------------------------------------------------------------------------------------------------------------
+
 function contains($haystack, $needle) {
   if (strpos($haystack, $needle) > -1) {
     return true;
@@ -170,7 +302,5 @@ function contains($haystack, $needle) {
     return false;
   }
 }
-
-// ============================================================================
 
 ?>
